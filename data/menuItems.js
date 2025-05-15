e1be372dda1dadf2b1b58c76249d5e3791c19d4a
matthias <matthias.giraudeau@newmips.com>
2025-05-15 14:18:21 +0200

    first commit

---
+++ data/menuItems.js
@@ -0,0 +1,8 @@
+const menuItems = [
+	{ path: "/", title: "Home", isActive: true },
+	{ path: "/about-me", title: "About", isActive: false },
+	{ path: "/references", title: "References", isActive: false },
+	{ path: "/contact-me", title: "Contact", isActive: false },
+];
+
+module.exports = menuItems;
